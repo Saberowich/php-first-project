4d26f94ae4d50ef895d89c2c4df82264e3bf9bc6
Bohdan Babych <babychbohdan0@gmail.com>
2024-03-12 14:37:12 +0100

    add

--- babychbohdan1-5zadania.php
+++ babychbohdan1-5zadania.php
@@ -12,7 +12,7 @@
 <body>
     <header><h1>FERRARI</h1></header>
     <section>
-     <img src="img/Untitled.jpg" alt="Ferrari">
+     <img src="https://en.wikipedia.org/wiki/File:2021_Ferrari_F8_Tributo.jpg" alt="Ferrari">
      <p>Ferrari - wloski producent samoshodów sportowych</p>
      <p>Przedsiębiorstwo zalozone zostalo w 1939 roku przez Enzo Ferrari,
      od 1947 roku produkuje auta marki Ferrari. W latach 1969 do 2016 nalezalo do koncernu Fiata.</p>
