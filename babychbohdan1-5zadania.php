e2e0f3072f030f175028af641da4ece00e8bf9f1
Bohdan Babych <babychbohdan0@gmail.com>
2024-03-12 14:34:15 +0100

    a

---
+++ babychbohdan1-5zadania.php
@@ -0,0 +1,433 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    hr {
+  color: blue;
+}</style>
+</head>
+<body>
+    <header><h1>FERRARI</h1></header>
+    <section>
+     <img src="img/Untitled.jpg" alt="Ferrari">
+     <p>Ferrari - wloski producent samoshodów sportowych</p>
+     <p>Przedsiębiorstwo zalozone zostalo w 1939 roku przez Enzo Ferrari,
+     od 1947 roku produkuje auta marki Ferrari. W latach 1969 do 2016 nalezalo do koncernu Fiata.</p>
+    </section>
+   <section>
+    <hr>
+    <h2>Modele:</h2>
+    <ul>
+        <li>488 Pista Spider</li>
+        <li>California</li>
+        <li>F12 Berinetta</li>
+        <li>430</li>
+        <li>458</li>
+    </ul>
+   </section>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    body
+    <header>
+      Header
+      <nav>nav</nav>
+    </header>
+    <section>
+      Section
+      <nav>nav</nav>
+      <aside>aside</aside>
+    </section>
+    <section>
+      Section
+      <header>Header</header>
+      <article>
+        Article
+        <header>header</header>
+        <div>div</div>
+        <aside>aside</aside>
+        <p>p</p>
+      </article>
+      <article>
+        Article
+        <header>header</header>
+        <div>div</div>
+        <aside>aside</aside>
+        <p>p</p>
+      </article>
+      <footer>footer</footer>
+    </section>
+
+    <footer>footer</footer>
+  </body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <style>
+    .tablica {
+  border: 2px solid black;
+}
+.black {
+  background-color: black;
+  width: 100px;
+  height: 100px;
+}
+.white {
+  background-color: white;
+  width: 100px;
+  height: 100px;
+}
+   </style>
+    <title>Document</title>
+    <style></style>
+  </head>
+  <body>
+    <table class="tablica">
+      <!-- 1 -->
+      <tr>
+        <td class="black"></td>
+        <td class="white"></td>
+        <td class="black"></td>
+        <td class="white"></td>
+        <td class="black"></td>
+      </tr>
+      <!-- 2 -->
+      <tr>
+        <td class="white"></td>
+        <td class="black"></td>
+        <td class="white"></td>
+        <td class="black"></td>
+        <td class="white"></td>
+      </tr>
+      <!-- 3 -->
+      <tr>
+        <td class="black"></td>
+        <td class="white"></td>
+        <td class="black"></td>
+        <td class="white"></td>
+        <td class="black"></td>
+      </tr>
+      <!-- 4 -->
+      <tr>
+        <td class="white"></td>
+        <td class="black"></td>
+        <td class="white"></td>
+        <td class="black"></td>
+        <td class="white"></td>
+      </tr>
+      <!-- 5 -->
+      <tr>
+        <td class="black"></td>
+        <td class="white"></td>
+        <td class="black"></td>
+        <td class="white"></td>
+        <td class="black"></td>
+      </tr>
+    </table>
+  </body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+        .tablica-header {
+  background-color: grey;
+  border: 2px solid black;}
+        .opcja1 {
+  background-color: darkgrey;}
+
+    </style>
+    <title>Document</title>
+  </head>
+  <body>
+    <table class="tablica">
+      <tr class="tablica-header">
+        <td>Imie i Nazwisko</td>
+        <td>Ulica Nr mieszkania</td>
+        <td>Zawod</td>
+        <td>Wiek</td>
+        <td>Wzrost</td>
+        <td>Miasto</td>
+      </tr>
+      <tr class="opcja1">
+        <td>Jadwiga bilecka</td>
+        <td>Kwiatowa 29/6</td>
+        <td>filolog</td>
+        <td>48</td>
+        <td>156</td>
+        <td>Radom</td>
+      </tr>
+      <tr>
+        <td>Rafal Bobek</td>
+        <td>Kwiatowa 32/6</td>
+        <td>kartograf</td>
+        <td>54</td>
+        <td>190</td>
+        <td>Radom</td>
+      </tr>
+      <tr class="opcja1">
+        <td>Robert Bobekowski</td>
+        <td>Kwiatowa 29/5</td>
+        <td>kartograf</td>
+        <td>32</td>
+        <td>175</td>
+        <td>Radom</td>
+      </tr>
+      <tr>
+        <td>Rafal Bobikowski</td>
+        <td>Kwiatowa 31/3</td>
+        <td>kartograf</td>
+        <td>32</td>
+        <td>184</td>
+        <td>Warszawa</td>
+      </tr>
+      <tr class="opcja1">
+        <td>Rafal Brobek</td>
+        <td>Kwiatowa 26/3</td>
+        <td>kartograf</td>
+        <td>42</td>
+        <td>160</td>
+        <td>Warszawa</td>
+      </tr>
+      <tr>
+        <td>WiktorBrudziak</td>
+        <td>Kwiatowa 39/13</td>
+        <td>nauczyciel</td>
+        <td>47</td>
+        <td>179</td>
+        <td>Warszawa</td>
+      </tr>
+      <tr class="opcja1">
+        <td>Boleslaw Brynski</td>
+        <td>Kwiatowa 34 6</td>
+        <td>geograf</td>
+        <td>28</td>
+        <td>191</td>
+        <td>Warszawa</td>
+      </tr>
+      <tr>
+        <td>Jadwiga Chrilecka</td>
+        <td>Kwiatowa 24 5</td>
+        <td>geodeta</td>
+        <td>24</td>
+        <td>180</td>
+        <td>Warszawa</td>
+      </tr>
+      <tr class="opcja1">
+        <td>Liliana Ciupala</td>
+        <td>Kwiatowa 33 3</td>
+        <td>inzynier</td>
+        <td>51</td>
+        <td>170</td>
+        <td>Warszawa</td>
+      </tr>
+      <tr>
+        <td>Katarzyna Dawro</td>
+        <td>Kwiatowa 33 11</td>
+        <td>geograf</td>
+        <td>53</td>
+        <td>187</td>
+        <td>Warszawa</td>
+      </tr>
+      <tr class="opcja1">
+        <td>Jadwiga Derilecka</td>
+        <td>Kwiatowa 30 7</td>
+        <td>geodeta</td>
+        <td>50</td>
+        <td>188</td>
+        <td>Warszawa</td>
+      </tr>
+    </table>
+  </body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+      .task5Form {
+        margin: auto;
+        width: 30%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: #f6f6f6;
+      }
+      .wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+      }
+      .input {
+        width: 70%;
+        height: 100%;
+      }
+      .input-tel1 {
+        width: 5%;
+        height: 20px;
+      }
+      .input-tel2 {
+        width: 62.25%;
+        height: 20px;
+      }
+      .star {
+        color: red;
+      }
+      .input-container {
+        display: inline;
+      }
+      .imie {
+        margin-right: 58px;
+      }
+      .nazwisko {
+        margin-right: 24px;
+      }
+      .telefon {
+        margin-right: 38px;
+      }
+      .kod {
+        margin-right: 60px;
+      }
+      .miasto {
+        margin-right: 44px;
+      }
+      .adres {
+        margin-right: 50px;
+      }
+      .email {
+        margin-right: 50px;
+      }
+      .haslo {
+        margin-right: 54px;
+      }
+      .checkbox {
+        background-color: #08847c;
+      }
+      .link {
+        color: #08847c;
+      }
+      .button {
+        height: 40px;
+        background-color: #08847c;
+        color: #f6f6f6;
+      }
+    </style>
+  </head>
+  <body class="body">
+    <form class="task5Form">
+      <h3>Rejestracja nowego konta</h3>
+      <div class="wrapper">
+        <div class="input-container">
+          <label class="imie" for="imie">Imię:</label>
+          <input class="input" type="text" id="imie" placeholder="imię" />
+          <span class="star"> *</span>
+        </div>
+        <div class="input-container">
+          <label class="nazwisko" for="nazwisko">Nazwisko:</label>
+          <input
+            class="input"
+            type="text"
+            id="nazwisko"
+            placeholder="nazwisko"
+          />
+          <span class="star"> *</span>
+        </div>
+        <div class="input-container">
+          <label class="telefon" for="telefon">Telefon:</label>
+          <input
+            class="input-tel1"
+            type="text"
+            id="telefon"
+            placeholder="nazwisko"
+            value="48"
+          />
+          <input
+            class="input-tel2"
+            type="text"
+            id="Telefon"
+            placeholder="Numer telefonu"
+          />
+          <span class="star"> *</span>
+        </div>
+        <div class="input-container">
+          <label class="kod" for="kod">Kod:</label>
+          <input
+            class="input"
+            type="text"
+            id="kod"
+            placeholder="Kod pocztowy"
+          />
+          <span class="star"> *</span>
+        </div>
+        <div class="input-container">
+          <label class="miasto" for="miasto">Miasto:</label>
+          <input class="input" type="text" id="miasto" placeholder="Miasto" />
+          <span class="star"> *</span>
+        </div>
+        <div class="input-container">
+          <label class="adres" for="adres">Adres:</label>
+          <input class="input" type="text" id="adres" placeholder="Adres" />
+          <span class="star"> *</span>
+        </div>
+        <div class="input-container">
+          <label class="email" for="email">e-mail:</label>
+          <input
+            class="input"
+            type="text"
+            id="email"
+            placeholder="Adres email"
+          />
+          <span class="star"> *</span>
+        </div>
+        <div class="input-container">
+          <label class="haslo" for="haslo">Hasło:</label>
+          <input class="input" type="password" id="haslo" placeholder="Hasło" />
+          <span class="star"> *</span>
+        </div>
+        <div class="input-container">
+          <label for="powHaslo">Powtórz hasło:</label>
+          <input
+            class="input"
+            type="password"
+            id="powHaslo"
+            placeholder="Powtórz hasło"
+          />
+          <span class="star"> *</span>
+        </div>
+        <div class="input-container">
+          <div class="checkbox-wrap">
+            <input type="checkbox" id="checkbox" />
+            <label for="checkbox"
+              >Zapoznałem się z treścią
+              <a class="link" href="">relugaminu sklepu internetowego</a>
+            </label>
+            <p><span class="star"> *</span> - pola obowiązkowe</p>
+          </div>
+        </div>
+        <div class="input-container"></div>
+        <button type="button" class="button">Dalej</button>
+      </div>
+    </form>
+  </body>
+</html>
